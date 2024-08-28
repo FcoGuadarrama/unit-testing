8ed23a5511d792fdb7115faf509a4c69af3a8327
FcoGuadarrama <francisco.guadarrama@maus.mx>
2024-08-28 10:49:58 -0600

    fourth commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,15 +6,20 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(): void
     {
         $hola = 1924;
 
-        for($i = 0; $i < $hola; $i++) {
-            for($j = 0; $j < $i - 1; $j++ ){
+        for ($i = 0; $i < $hola; $i++) {
+            for ($j = 0; $j < $i - 1; $j++) {
                 echo '$';
             }
         }
+    }
+
+    public function testing(): void
+    {
+        echo 'este es un test';
     }
 
 
