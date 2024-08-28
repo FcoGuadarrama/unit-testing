30c63da94f17dfa22c1bbd43d18ebae452cd0488
FcoGuadarrama <francisco.guadarrama@maus.mx>
2024-08-28 10:47:51 -0600

    third commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,7 +8,13 @@ class AdminController extends Controller
 {
     public function index()
     {
-        return "index";
+        $hola = 1924;
+
+        for($i = 0; $i < $hola; $i++) {
+            for($j = 0; $j < $i - 1; $j++ ){
+                echo '$';
+            }
+        }
     }
 
 
